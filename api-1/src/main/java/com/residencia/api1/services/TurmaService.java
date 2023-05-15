0aa6a7ec293e58b8ced3b99f89c4a93fbfb2496c
João Marco Piccoli Cardoso <127253938+JoaoMarcoPiccoliCardoso@users.noreply.github.com>
2023-05-15 16:16:35 -0300

    Services

---
+++ api-1/src/main/java/com/residencia/api1/services/TurmaService.java
@@ -0,0 +1,42 @@
+package com.residencia.api1.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.residencia.api1.entities.Turma;
+import com.residencia.api1.repositories.TurmaRepository;
+
+@Service
+public class TurmaService {
+	@Autowired
+	TurmaRepository turmaRepository;
+	
+	public List<Turma> getAllTurmaes() {
+		return turmaRepository.findAll();
+	}
+	
+	public Turma getTurmaById(Integer id) {
+		return turmaRepository.findById(id).orElse(null);
+	}
+	
+	public Turma saveTurma(Turma turma) {
+		return turmaRepository.save(turma);
+	}
+	
+	public Turma updateTurma(Turma turma, Integer id) {
+		return turmaRepository.save(turma);
+	}
+	
+	public boolean deleteTurma(Integer id) {
+		turmaRepository.deleteById(id);
+		Turma turmaDeletado = turmaRepository.findById(id).orElse(null);
+		if (null == turmaDeletado) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
