4ad9b17a8d6009353e52d87813373aa487ae6d7e
RonaldoAglio <ronaldinho_peixes@hotmail.com>
2023-05-16 21:05:31 -0300

    inclusao do jsonIdentityInfo

--- api-1/src/main/java/com/residencia/api1/entities/Instrutor.java
+++ api-1/src/main/java/com/residencia/api1/entities/Instrutor.java
@@ -2,6 +2,9 @@ package com.residencia.api1.entities;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,6 +13,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "id"
+        )
 
 @Entity
 @Table(name = "instrutor")
--- api-1/src/main/java/com/residencia/api1/entities/Turma.java
+++ api-1/src/main/java/com/residencia/api1/entities/Turma.java
@@ -1,5 +1,8 @@
 package com.residencia.api1.entities;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,6 +11,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "id"
+        )
 
 @Entity
 @Table(name = "turma")
